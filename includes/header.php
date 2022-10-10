55499c086790108ce3fe6d255eb365db5b0b03f5
mehwish <mehwishgul715@gmail.com>
2022-10-10 11:36:29 +0500

    commit

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Redpaal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+// Put Navbar and sidebar here
