86746a671dc869dcf9cf539569edf5f5a13c7a21
mehwish <mehwishgul715@gmail.com>
2022-11-13 11:04:22 +0500

    commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
-
-        
+// ==================data table
+    $(document).ready( function () {
+        $('#dashboard-table').DataTable();
+    } );    
 //    Charrts 1
 var xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
 var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
@@ -11,8 +13,8 @@ new Chart("myChart", {
         datasets: [{
             fill: false,
             lineTension: 0,
-            backgroundColor: "rgba(0,0,255,1.0)",
-            borderColor: "rgba(0,0,255,0.1)",
+            backgroundColor: "#7367f0",
+            borderColor: "#7367f0",
             data: yValues
         }]
     },
@@ -37,7 +39,7 @@ new Chart("myChart2", {
         datasets: [{
             fill: false,
             lineTension: 0,
-            backgroundColor: "rgba(0,0,255,1.0)",
+            backgroundColor: "#7367f0",
             borderColor: "rgba(0,0,255,0.1)",
             data: yValues
         }]
